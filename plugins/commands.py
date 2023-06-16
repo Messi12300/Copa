ac7cdb60edb44e870f0fbe50959ae8c21c3a5ebd
ɢᴛ ᠰ ʜᴇʀᴏ <92996711+TGGTHERO@users.noreply.github.com>
2023-06-16 19:20:07 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -233,7 +233,7 @@ async def start(client, message):
         chat_id=message.from_user.id,
         file_id=file_id,
         caption=f_caption,
-        reply_markup=InlineKeyboardMarkup( [ [ InlineKeyboardButton('🚸 ᴅᴇʟᴇᴛᴇ', callback_data='close_data') ] ] ),
+        reply_markup=InlineKeyboardMarkup(buttons)
         protect_content=True if pre == 'filep' else False,
         )
                     
